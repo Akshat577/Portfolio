8ba57820ba10b5f5788141d565285e5aea961b85
Akshat577 <akshatjain5773@gmail.com>
2025-07-05 01:40:39 +0530

    Implements basic website structure and styling
    Sets up the basic structure for the portfolio website using React and Tailwind CSS.
    
    - Integrates Google Analytics for tracking user behavior.
    - Adds basic sections like Home, About, Experience, Projects, Coding, Certificates, Contact, and Footer.
    - Establishes a basic layout and styling.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#F0EFE8',
+          textlight: '#262010',
+          primarydark: '#0B0A08',
+          textdark1: '#D7CAA5',
+          textdark2: '#F0EFE8',
+        },
+      },
+    },
+    plugins: [],
+  }
